7aa942b9ddd94c2c66766a0726a813eae8bb2878
doorbash <milad.doorbash@gmail.com>
2022-09-19 04:48:29 +0430

    removed crossbeam_channel, all tokio now

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod pm2;
 use actix_files::Files;
 use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, Result, web, rt};
 use askama::Template;
-use crossbeam_channel::{select, Sender};
-use std::{collections::HashMap, thread, time::{Duration}};
+use std::{collections::HashMap, process, time::{Duration}};
 use uuid::Uuid;
+use tokio::sync::mpsc::{Sender, channel};
 
 #[derive(Template)]
 #[template(path = "script.js", escape = "none")]
@@ -38,11 +38,11 @@ async fn js_handler(_: HttpRequest) -> Result<HttpResponse> {
 async fn logs_handler(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     let (res, mut session, _) = actix_ws::handle(&req, stream)?;
 
-    let client_ch_s = req.app_data::<Sender<Client>>().unwrap().clone();
-    let removed_client_ch_s = req.app_data::<Sender<Uuid>>().unwrap().clone();
+    let clients_ch_s = req.app_data::<Sender<Client>>().unwrap().clone();
+    let removed_clients_ch_s = req.app_data::<Sender<Uuid>>().unwrap().clone();
 
-    let (stats_ch_s, stats_ch_r) = crossbeam_channel::bounded::<String>(1);
-    let (logs_ch_s, logs_ch_r) = crossbeam_channel::bounded::<String>(10);
+    let (stats_ch_s, mut stats_ch_r) = channel::<String>(1);
+    let (logs_ch_s, mut logs_ch_r) = channel::<String>(10);
 
     let uuid = Uuid::new_v4();
 
@@ -52,27 +52,23 @@ async fn logs_handler(req: HttpRequest, stream: web::Payload) -> Result<HttpResp
         logs_ch: logs_ch_s,
     };
 
-    client_ch_s.send(client).unwrap();
-
     rt::spawn(async move {
+        clients_ch_s.send(client).await;
         loop {
-            select! {
-                recv(stats_ch_r) -> message => {
+            tokio::select! {
+                message = stats_ch_r.recv() => {
                     if session.text(message.unwrap()).await.is_err() {
-                        removed_client_ch_s.send(uuid).unwrap();
+                        removed_clients_ch_s.send(uuid).await;
                         session.close(None).await;
                         return
                     }
-                }
-                recv(logs_ch_r) -> message => {
+                },
+                message = logs_ch_r.recv() => {
                     if session.text(message.unwrap()).await.is_err() {
-                        removed_client_ch_s.send(uuid).unwrap();
+                        removed_clients_ch_s.send(uuid).await;
                         session.close(None).await;
                         return;
                     }
-                }
-                default => {
-                    tokio::task::yield_now().await;
                 }
             }
         }
@@ -82,44 +78,49 @@ async fn logs_handler(req: HttpRequest, stream: web::Payload) -> Result<HttpResp
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), std::io::Error> {
     // pretty_env_logger::init();
 
-    let (stats_ch_s, stats_ch_r) = crossbeam_channel::bounded::<String>(1);
-    let (logs_ch_s, logs_ch_r) = crossbeam_channel::bounded::<String>(10);
-    let (clients_ch_s, clients_ch_r) = crossbeam_channel::bounded::<Client>(10);
-    let (removed_clients_ch_s, removed_clients_ch_r) = crossbeam_channel::bounded::<Uuid>(10);
+    ctrlc::set_handler(move || {
+        process::exit(0);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    let (stats_ch_s, mut stats_ch_r) = channel::<String>(1);
+    let (logs_ch_s, mut logs_ch_r) = channel::<String>(10);
+    let (clients_ch_s, mut clients_ch_r) = channel::<Client>(10);
+    let (removed_clients_ch_s, mut removed_clients_ch_r) = channel::<Uuid>(10);
 
     pm2::PM2::start(stats_ch_s, logs_ch_s, Duration::from_secs(3));
 
-    thread::spawn(move || {
+    tokio::task::spawn(async move {
         let mut clients: HashMap<Uuid, Client> = HashMap::new();
         loop {
-            select! {
-                recv(stats_ch_r) -> data => {
+            tokio::select! {
+                data = stats_ch_r.recv() => {
                     let data = data.unwrap();
                     for client in clients.values() {
-                        select! {
-                            send(client.stats_ch, data.clone()) -> _ => (),
-                            default => ()
+                        tokio::select! {
+                            _ = client.stats_ch.send(data.clone()) => {},
+                            else => ()
                         }
                     }
-                }
-                recv(logs_ch_r) -> data => {
+                },
+               data = logs_ch_r.recv() => {
                     let data = data.unwrap();
                     for client in clients.values() {
-                        select! {
-                            send(client.logs_ch, data.clone()) -> _ => (),
-                            default => ()
+                        tokio::select! {
+                            _ = client.logs_ch.send(data.clone()) => (),
+                            else => ()
                         }
                     }
-                }
-                recv(removed_clients_ch_r) -> uuid => {
+                },
+                uuid = removed_clients_ch_r.recv() => {
                     let uuid = uuid.unwrap();
                     println!("client disconnected: {}", uuid);
                     clients.remove(&uuid);
-                }
-                recv(clients_ch_r) -> client => {
+                },
+                client = clients_ch_r.recv() => {
                     let client = client.unwrap();
                     println!("client connected: {}", client.uuid);
                     clients.insert(client.uuid, client);
@@ -138,7 +139,6 @@ async fn main() -> std::io::Result<()> {
             .service(Files::new("/", "./static/").index_file("index.html"))
     })
     .bind(("0.0.0.0", 6060))?
-    .workers(4)
-    .run()
-    .await
+    .workers(10)
+    .run().await
 }
--- src/pm2.rs
+++ src/pm2.rs
@@ -1,12 +1,13 @@
 use core::time;
-use crossbeam_channel::{after, select, Sender};
 use serde::Serialize;
 use serde_json::Value;
-use std::io::{BufRead, BufReader};
-use std::ops::Index;
-use std::process::{Command, Output, Stdio};
-use std::time::{SystemTime, UNIX_EPOCH};
-use std::{str, thread};
+use std::{
+    io::{BufRead, BufReader},
+    process::{Command, Output, Stdio},
+    str,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
+use tokio::{pin, sync::mpsc::Sender, task::JoinHandle, time::sleep};
 
 pub struct PM2 {}
 
@@ -41,133 +42,132 @@ struct Message<T> {
 }
 
 impl PM2 {
-    pub fn start(stats_chan: Sender<String>, logs_chan: Sender<String>, interval: time::Duration) {
-        thread::spawn(move || {
-            loop {
-                let mut list: Vec<Stats> = vec![];
-                let output: Output = Command::new("pm2")
-                    .arg("jlist")
-                    .output()
-                    .expect("failed to execute process");
+    pub fn start(
+        stats_chan: Sender<String>,
+        logs_chan: Sender<String>,
+        interval: time::Duration,
+    ) -> (JoinHandle<()>, JoinHandle<()>) {
+        return (
+            tokio::task::spawn(async move {
+                loop {
+                    let mut list: Vec<Stats> = vec![];
+                    let output: Output = Command::new("pm2")
+                        .arg("jlist")
+                        .output()
+                        .expect("failed to execute process");
 
-                if let Value::Array(arr) =
-                    serde_json::from_str(str::from_utf8(&output.stdout).unwrap()).unwrap()
-                {
-                    for obj in arr {
-                        let mut stats = Stats {
-                            name: String::from(""),
-                            id: 0,
-                            pid: 0,
-                            uptime: 0,
-                            status: String::from(""),
-                            restart: 0,
-                            user: String::from(""),
-                            cpu: 0f64,
-                            mem: 0,
-                        };
-                        if let Value::String(name) = obj["name"].clone() {
-                            // println!("name = {}", name);
-                            stats.name = name
+                    if let Value::Array(arr) =
+                        serde_json::from_str(str::from_utf8(&output.stdout).unwrap()).unwrap()
+                    {
+                        for obj in arr {
+                            let mut stats = Stats {
+                                name: String::from(""),
+                                id: 0,
+                                pid: 0,
+                                uptime: 0,
+                                status: String::from(""),
+                                restart: 0,
+                                user: String::from(""),
+                                cpu: 0f64,
+                                mem: 0,
+                            };
+                            if let Value::String(name) = obj["name"].clone() {
+                                stats.name = name
+                            }
+                            if let Value::Number(id) = obj["pm_id"].clone() {
+                                stats.id = id.as_i64().unwrap();
+                            }
+                            if let Value::Number(pid) = obj["pid"].clone() {
+                                stats.pid = pid.as_i64().unwrap();
+                            }
+                            if let Value::Object(pm2_env) = &obj["pm2_env"] {
+                                if let Value::Number(uptime) = pm2_env["pm_uptime"].clone() {
+                                    stats.uptime = uptime.as_i64().unwrap()
+                                }
+                                if let Value::String(status) = pm2_env["status"].clone() {
+                                    stats.status = status;
+                                }
+                                if let Value::Number(restart) = pm2_env["restart_time"].clone() {
+                                    stats.restart = restart.as_i64().unwrap();
+                                }
+                                if let Value::String(user) = pm2_env["username"].clone() {
+                                    stats.user = user;
+                                }
+                            }
+                            if let Value::Object(monit) = &obj["monit"] {
+                                if let Value::Number(cpu) = monit["cpu"].clone() {
+                                    stats.cpu = cpu.as_f64().unwrap();
+                                }
+                                if let Value::Number(mem) = monit["memory"].clone() {
+                                    stats.mem = mem.as_i64().unwrap();
+                                }
+                            }
+                            list.push(stats);
                         }
-                        if let Value::Number(id) = obj["pm_id"].clone() {
-                            // println!("id = {}", id);
-                            stats.id = id.as_i64().unwrap();
-                        }
-                        if let Value::Number(pid) = obj["pid"].clone() {
-                            // println!("pid = {}", pid);
-                            stats.pid = pid.as_i64().unwrap();
-                        }
-                        if let Value::Object(pm2_env) = &obj["pm2_env"] {
-                            if let Value::Number(uptime) = pm2_env["pm_uptime"].clone() {
-                                // println!("uptime = {}", uptime);
-                                stats.uptime = uptime.as_i64().unwrap()
-                            }
-                            if let Value::String(status) = pm2_env["status"].clone() {
-                                // println!("status = {}", status);
-                                stats.status = status;
-                            }
-                            if let Value::Number(restart) = pm2_env["restart_time"].clone() {
-                                // println!("restart = {}", restart);
-                                stats.restart = restart.as_i64().unwrap();
-                            }
-                            if let Value::String(user) = pm2_env["username"].clone() {
-                                // println!("user = {}", user);
-                                stats.user = user;
-                            }
-                        }
-                        if let Value::Object(monit) = &obj["monit"] {
-                            if let Value::Number(cpu) = monit["cpu"].clone() {
-                                // println!("cpu = {}", cpu);
-                                stats.cpu = cpu.as_f64().unwrap();
-                            }
-                            if let Value::Number(mem) = monit["memory"].clone() {
-                                // println!("mem = {}", mem);
-                                stats.mem = mem.as_i64().unwrap();
-                            }
-                        }
-                        // println!("{:?}", stats);
-                        list.push(stats);
                     }
+                    let data = Message {
+                        Type: String::from("stats"),
+                        Data: list,
+                        Time: SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis(),
+                    };
+                    let sleep = sleep(Duration::from_secs(1));
+                    pin!(sleep);
+                    tokio::select! {
+                        _ = stats_chan.send(serde_json::to_string(&data).unwrap()) => {},
+                        _ = &mut sleep => {
+                            println!("sleep timeout!");
+                            break;
+                        }
+                    }
+                    tokio::time::sleep(interval).await;
                 }
-                // println!("trying to send...");
-                let data = Message {
-                    Type: String::from("stats"),
-                    Data: list,
-                    Time: SystemTime::now()
-                        .duration_since(UNIX_EPOCH)
-                        .unwrap()
-                        .as_millis(),
-                };
-                select! {
-                    send(stats_chan, serde_json::to_string(&data).unwrap()) -> _ => {}
-                    recv(after(time::Duration::from_secs(3))) -> _ => {}
-                    // default => {}
+            }),
+            tokio::task::spawn_blocking(move || {
+                let child = Command::new("pm2")
+                    .arg("logs")
+                    .arg("--format")
+                    .arg("--timestamp")
+                    .stdin(Stdio::piped())
+                    .stdout(Stdio::piped())
+                    .spawn()
+                    .unwrap();
+                let mut br = BufReader::new(child.stdout.unwrap());
+                loop {
+                    let mut line = String::new();
+                    br.read_line(&mut line).unwrap();
+
+                    if None == line.find("timestamp=") {
+                        continue;
+                    }
+
+                    let idx1 = line.find(' ').unwrap();
+                    let idx2 = idx1 + line[idx1 + 1..].find(' ').unwrap() + 1;
+                    let idx3 = idx2 + line[idx2 + 1..].find(' ').unwrap() + 1;
+                    let idx4 = idx3 + line[idx3 + 1..].find(' ').unwrap() + 1;
+
+                    let data = Message {
+                        Type: String::from("log"),
+                        Data: LogsData {
+                            app: line[idx1 + 5..idx2].to_string(),
+                            id: line[idx2 + 4..idx3].to_string(),
+                            message: line[idx4 + 9..].to_string(),
+                            time: format!("{} {}", &line[10..20], &line[21..idx1]),
+                            Type: line[idx3 + 6..idx4].to_string(),
+                        },
+                        Time: SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis(),
+                    };
+                    let lg_ch = logs_chan.clone();
+                    let x = tokio::task::spawn(async move {
+                        lg_ch.send(serde_json::to_string(&data).unwrap()).await;
+                    });
                 }
-                thread::sleep(interval);
-            }
-        });
-
-        thread::spawn(move || {
-            let child = Command::new("pm2")
-                .arg("logs")
-                .arg("--format")
-                .arg("--timestamp")
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .spawn()
-                .unwrap();
-            let mut br = BufReader::new(child.stdout.unwrap());
-            loop {
-                let mut line = String::new();
-                br.read_line(&mut line).unwrap();
-
-                if None == line.find("timestamp=") {
-                    continue;
-                }
-
-                let idx1 = line.find(' ').unwrap();
-                let idx2 = idx1 + line[idx1+1..].find(' ').unwrap() + 1;
-                let idx3 = idx2 + line[idx2+1..].find(' ').unwrap() + 1;
-                let idx4 = idx3 + line[idx3+1..].find(' ').unwrap() + 1;
-
-                let data = Message {
-                    Type: String::from("log"),
-                    Data: LogsData {
-                        app: line[idx1+5..idx2].to_string(),
-                        id: line[idx2+4..idx3].to_string(),
-                        message: line[idx4+9..].to_string(),
-                        time: format!("{} {}", &line[10..20], &line[21..idx1]),
-                        Type: line[idx3+6..idx4].to_string(),
-                    },
-                    Time: SystemTime::now()
-                        .duration_since(UNIX_EPOCH)
-                        .unwrap()
-                        .as_millis(),
-                };
-                logs_chan
-                    .send(serde_json::to_string(&data).unwrap())
-                    .unwrap()
-            }
-        });
+            }),
+        );
     }
 }
